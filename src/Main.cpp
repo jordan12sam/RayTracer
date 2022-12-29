f053283cc19f47ff4924099b95e8d60904ae6ba6
Sam Jordan <jordan12sam@gmail.com>
2022-12-29 15:12:29 +0000

    Vertex buffers

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,7 +9,7 @@ const unsigned int SCR_HEIGHT = 480;
 
 int main()
 {
-    // glfw: initialize and configure
+    // glfw: initialise and configure
     if (!glfwInit())
         return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -17,7 +17,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // glfw window creation
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "RayTracer", NULL, NULL);
     if (window == NULL)
     {
         glfwTerminate();
@@ -25,17 +25,36 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
+    // glew: initialise
+    if (glewInit() != GLEW_OK)
+        return -1;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.0f, 0.5f
+    };
+
+    // vertex buffer
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(positions[0]) * 2, 0);
+
     // render loop
     while (!glfwWindowShouldClose(window))
     {
-        // render colour
-        glClearColor(.0f, 1.0f, 0.0f, 1.0f);
+        //render        
         glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // swap front and back buffers
         glfwSwapBuffers(window);
 
-        // poll for and process events
+        // poll for process events6 * sizeof(float)
         glfwPollEvents();
     }
 
